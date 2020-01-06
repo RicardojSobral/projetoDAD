2fcba64808ed4056c2f0b41af3bd0866f8e26f7f
Daniel <danielmatiasvieira@gmail.com>
2020-01-06 19:15:59 +0000

    US-12 Done

--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -1,2 +1,4 @@
-Hello <strong>TEST</strong>
-<p>hjtrjidzh</p>
+Hello <strong>{{ $user['name'] }}</strong>,
+<br>
+<p>An income movement of {{ $value }}€ as added to your wallet. <a href="{{ env('APP_URL') }}">Sign-in Here</a> to get more details</p>
+<p>Best Regards</p>
